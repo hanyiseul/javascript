384eaa033160071e74e24ec180db20a086ae8c58
master <seulli_hys@daum.net>
2021-01-20 17:53:13 +0900

    for

---
+++ for.js
@@ -0,0 +1,78 @@
+// for 단일문
+for(var i = 1; i<=10; i++){
+	document.write(i+"seulli<br>");
+}
+
+for(var i=0; i<10; i++){
+	document.write("i="+i, "<br>");
+}
+document.write("종료 i =" + i+"<br>");
+
+for(var i=0; i<10; i++){
+	document.write((i+1)+"<br>");
+}
+
+//별찍기
+var result="";
+for(var i=0; i<10; i++){
+	result+="*";
+}
+document.write("result="+result+"<br>");
+
+//배열+ for 활용
+var data=["변수","연산자","형변환","조건문if","조건문switch","반복문for","반복문while","함수","클래스"];
+console.log(data.length);
+for(var i=0; i<data.length; i++){
+	document.write(i+"번째 내용="+data[i],"<br>");
+}
+
+//continue
+for(var i=1; i<=10; i++){
+	continue;
+	document.write(i+"<br>");
+}
+document.write("최종"+i+"<br>");
+
+//break
+for(var i=1; i<=10; i++){
+	break;
+	document.write(i+"<br>");
+}
+document.write("최종"+i+"<br>");
+
+
+// for 다중문
+
+//별찍기
+
+for(var i=0; i<5; i++){
+	var result="";
+	for(var m=0; m<=i;m++){
+		result+="*";
+	}
+	document.write(result,"<br>");
+}
+
+//별찍기 역방향
+for(var i=5; i>0; i--){
+	var result="";
+	for(var m=0; m<i; m++){
+		result+="*";
+	}
+	document.write(result,"<br>");
+}
+
+//for 응용
+
+//구구단
+var dan=3;
+for(i=1; i<=9; i++){
+	document.write(dan+"*"+i+"="+dan*i+"<br>");
+}
+//배열의 총 합
+var data=[10,20,30,40,50];
+var result=0;
+for(i=0; i<data.length; i++){
+	result+=data[i];
+}
+document.write(result);
