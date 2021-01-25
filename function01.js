7de9ae6ace131c1222df83f0861a310993916e9b
master <seulli_hys@daum.net>
2021-01-25 16:04:32 +0900

    함수

---
+++ function01.js
@@ -0,0 +1,12 @@
+for(var i=1; i<=5; i++)
+document.write(i+". function<br>");
+
+function ShowName(name){
+	for(var i=1; i<=5; i++)
+	document.write(i+"."+name+"입니다<br>");
+}
+
+//함수호출
+ShowName("안녕");
+ShowName("안녕2");
+ShowName("안녕3");
